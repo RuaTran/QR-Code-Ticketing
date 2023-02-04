a09a249b78fc41695abbe82f140e528257cb9b2c
Christopher Tran <110074141+RuaTran@users.noreply.github.com>
2023-02-04 14:59:39 -0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,19 @@
+
+<?php
+$q = $_GET['q'];
+$conn = mysqli_connect("162.241.216.197", "ucscjsac_sqluser", "RNjq8{mkVMvo", "ucscjsac_matsuri");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT `COL 2`, `COL 7` from `TABLE 1` where `COL 1` = '$q'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+$response = array($row["COL 2"],$row["COL 7"]);
+echo json_encode($response);
+}
+} else { echo 'invalid request!'; }
+$conn->close();
+?>
